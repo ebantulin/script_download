9b8035f29fa28b4ee3b71d2ad5720448aab7e459
ebantulin <164365276+ebantulin@users.noreply.github.com>
2024-05-14 20:05:14 +0000

    Update youtube_download.sh

--- youtube_download.sh
+++ youtube_download.sh
@@ -2,37 +2,44 @@
 #Made by ASIX_student
  
 check_tool() {
-	command -v $1 >/dev/null 2>&1
+    command -v $1 >/dev/null 2>&1
 }
 
 install_tools() {
-	if ! check_tool "yt-dlp"; then
-		echo "Instalando yt-dlp..."
-		pip install yt-dlp
-	fi
-	if ! check_tool "ffmpeg"; then
-		echo "Instalando  ffmpeg..."
-		sudo apt-get install ffmpeg
-	fi
+    if ! check_tool "yt-dlp"; then
+        echo "Instalando yt-dlp..."
+        pip install yt-dlp
+    fi
+    if ! check_tool "ffmpeg"; then
+        echo "Instalando ffmpeg..."
+        sudo apt-get install ffmpeg
+    fi
 }
 
 choose_format() {
-	echo "Formatos disponible para el video:"
-	yt-dlp -F $1
-	read -p "Escoge un formato de código para el video" format_code
+    echo "Formatos disponibles para el video:"
+    yt-dlp -F $1
+    read -p "Escoge un formato de código para el video: " format_code
 }
 
 main() {
-	install_tools
-	read -p "Pon la URL de Youtube: " youtube_url
-	choose_format $youtube_url
-	yt-dlp -f $format_ode $youtube_url
-	echo "Información de audio:"
-	ffmpeg -i *.mp3
-	echo "Información de video:"
-	ffmpeg -i *.mkv
-	mv *.mkv video_con_audio.mkv
-	echo "Hecho!"
+    install_tools
+    read -p "Pon la URL de Youtube: " youtube_url
+    choose_format $youtube_url
+    yt-dlp -f $format_code $youtube_url
+    
+    audio_file=$(ls | grep ".mp4" | head -n 1)
+    video_file=$(ls | grep ".mkv" | head -n 1)
+    
+    echo "Información de audio:"
+    ffmpeg -i $audio_file
+    
+    echo "Información de video:"
+    ffmpeg -i $video_file
+    
+    mv $video_file video_sin_audio.mkv
+    
+    echo "Hecho!"
 }
 
 main
